b1be37aa19d68e842efa5082a9c45dd5932d8d7d
Anthony Chaves <anthony@anthonychaves.net>
2009-11-16 14:27:40 -0500

    bookmark class

--- src/User.java
+++ src/User.java
@@ -1,10 +1,16 @@
 import javax.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.Serializable;
 
 @Entity
-public class User {
+public class User implements Serializable {
 	@Id int id;
 	String name;
 	String emailAddress;
+	
+	@OneToMany
+	List<Bookmark> bookmarks = new ArrayList<Bookmark>();
 	
 	@Version int version;
 	
