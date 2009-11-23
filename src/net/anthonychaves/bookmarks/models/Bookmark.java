4c087a6e308f88756f4fc488c883c04f8c4c825d
Anthony Chaves <anthony@anthonychaves.net>
2009-11-22 21:30:18 -0500

    updates

--- src/net/anthonychaves/bookmarks/models/Bookmark.java
+++ src/net/anthonychaves/bookmarks/models/Bookmark.java
@@ -1,9 +1,16 @@
+package net.anthonychaves.bookmarks.models;
+
 import javax.persistence.*;
+import org.apache.openjpa.persistence.*;
 import java.io.Serializable;
 
 @Entity
+@Table(name="bookmarks")
 public class Bookmark implements Serializable {
 	@Id long id;
 	String url;
 	int hitCount;
+	
+	@ManyToOne
+	User user;
 }
--- src/net/anthonychaves/bookmarks/models/User.java
+++ src/net/anthonychaves/bookmarks/models/User.java
@@ -1,9 +1,12 @@
+package net.anthonychaves.bookmarks.models;
+
 import javax.persistence.*;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.Serializable;
 
 @Entity
+@Table(name="users")
 public class User implements Serializable {
 	@Id 
 	@GeneratedValue(strategy=GenerationType.AUTO, generator="uuid-hex")
