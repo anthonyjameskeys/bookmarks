dad659bdcd71c51ba0d29e65a997fb634dbe0ce9
Anthony Chaves <anthony@anthonychaves.net>
2009-12-04 21:47:01 -0500

    working persistence deploy in glassfish

--- src/net/anthonychaves/bookmarks/web/UserController.java
+++ src/net/anthonychaves/bookmarks/web/UserController.java
@@ -2,14 +2,22 @@ package net.anthonychaves.bookmarks.web;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
+import javax.persistence.*;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+	@PersistenceUnit(unitName="bookmarksPU")
+	EntityManagerFactory emf;
 	
 	@RequestMapping(value="/hello/{id}")
 	@ResponseBody
 	public String helloUser(@PathVariable("id") int userId, @RequestHeader("User-Agent") String userAgent) {
 		return "you put in " + userId + " using " + userAgent;
+	}
+	
+	public void setEmf(EntityManagerFactory factory) {
+		this.emf = factory;
 	}
 }
