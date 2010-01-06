49031d76644a9f8f72dcc5d0a2bf8204cfdf4e29
Anthony Chaves <anthony@anthonychaves.net>
2010-01-06 13:42:54 -0500

    Added servlet filter for persistent login cookies

---
+++ src/net/anthonychaves/bookmarks/web/PersistentLoginFilter.java
@@ -0,0 +1,29 @@
+package net.anthonychaves.bookmarks.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.io.*;
+
+import net.anthonychaves.bookmarks.models.*;
+
+public class PersistentLoginFilter implements Filter {
+  
+  @Override
+  public void init(FilterConfig config) {
+    // nothing for now
+  }
+  
+  @Override
+  public void doFilter(ServletRequest request, 
+                       ServletResponse response, 
+                       FilterChain chain) throws IOException, ServletException {
+    
+    chain.doFilter(request, response);
+  }
+  
+  @Override
+  public void destroy() {
+    // nothing for now
+  }
+}
